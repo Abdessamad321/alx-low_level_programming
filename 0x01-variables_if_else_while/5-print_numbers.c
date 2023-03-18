f6f4bced8021554fd59bec9636b74f57596d82ad
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-03-17 18:02:18 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point Prints numbers of base 10 starting at 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+	printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
