a2b4b76fc2b5d33225c53b5fa3ec8d35d91e136d
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-03-17 18:33:52 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point Prints the alphabet lowercase using ascii backwards
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char chr;
+	
+	for (chr = 'z'; chr >= 'a'; chr--)
+	{
+	putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}
