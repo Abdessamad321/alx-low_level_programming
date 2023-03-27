c5ed1a4bfb82124ae9a0b50be15f65656cdf8f59
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-03-27 11:53:35 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,41 +1,27 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
- * @s: pointer to string
- *
- * Return: integer value of string, or 0 if no digits are found
+ * rev_string - reverses a string
+ * @s: the string to be reversed
  */
-int _atoi(char *s)
+void rev_string(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int digit_found = 0;
+	char ch;
+	int a, b, c;
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
+	a = 0;
+	b = 0;
 
-        if (*s >= '0' && *s <= '9')
-        {
-            digit_found = 1;
-            result = result * 10 + (*s - '0');
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+	c = b - 1;
 
-            /* Check for overflow */
-            if (result < 0)
-            {
-                if (sign == 1)
-                    return 2147483647;
-                else
-                    return -2147483648;
-            }
-        }
-        else if (digit_found)
-            break;
-
-        s++;
-    }
-
-    return (sign * result);
+	for (a = 0; a < b / 2; a++)
+	{
+		ch = s[a];
+		s[a] = s[c];
+		s[c--] = ch;
+	}
 }
