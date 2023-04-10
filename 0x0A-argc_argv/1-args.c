354660fecb3ae3441c8d9b736cfdd87d6acd6b5f
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-04-10 16:58:20 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,12 @@
  * main - program that prints the number of arguments passed into it.
  * @argc: argument counter
  * @argv: pointer to array of arguments
- * Rturn 0 = SUCCESS
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-	if (argc > 0)
-		printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 
 	return (0);
 
