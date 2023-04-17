60cb1aa1e06359228da95d41d5a795504e3f8e2e
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-04-17 15:21:49 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - define structure or new type named dog
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
