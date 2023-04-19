1534a6f1d33ceb9c328234442febab4b89546943
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-04-19 15:55:39 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,10 @@
  * print_name - it's a function that prints a name.
  * @name: name parameter
  * @f: the function
- *
  */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-
 	if (name != NULL && f != NULL)
 		f(name);
 }
