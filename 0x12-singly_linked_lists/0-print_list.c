44439c1aa5051653f805c3fe7a0413e55f4a4a1f
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-04-27 13:27:34 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* print_list - this function print elements of a linked list
+** @h: pointer to the head of linked list
+*** Return: the num of elements of the linked list
+*/
+
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	int elements = 0;
+
+	while (temp != NULL)
+	{
+		if (temp->str != NULL)
+		{
+			printf("[%d] %s\n", temp->len, temp->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		temp = temp->next;
+		elements++;
+	}
+	return (elements);
+}
