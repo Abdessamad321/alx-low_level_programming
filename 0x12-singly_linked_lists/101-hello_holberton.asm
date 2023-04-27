aef7c9a2a05f733c1912060924b331b2833c5939
Abdessamad321 <elazzaouiabdessamad4@gmail.com>
2023-04-27 14:00:12 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,3 +1,10 @@
+;; --------------------------------------------------------------------------------------
+	;;
+	;;  A 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
+	;;  Compiled using nasm and gcc
+	;;
+	;; --------------------------------------------------------------------------------------
+
 section .data
     hello: db 'Hello, Holberton', 0Ah
     hello_len: equ $-hello
